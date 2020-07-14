04c0b49608de0022b3b7cca450a137e74d963b6c
Luana Vicente <luana.vicente@inspira.com.br>
2020-07-14 20:58:08 -0300

    Dá pra adicionar itens e persiste quando dá f5

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,39 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css'
+import { uuid } from 'uuidv4'
 import { AiFillCheckCircle, AiFillDelete } from 'react-icons/ai'
 import { BsArrowCounterclockwise } from 'react-icons/bs'
 
-const TodoList: React.FC = () => {
+interface Todo {
+    id: string,
+    message: string,
+    done: boolean
+}
 
+const TodoList: React.FC = () => {
     
     const mock = [
         {
+            id: uuid(),
             message: "Testinho",
             done: false
         },
         {
-            message: "Testando 2",
-            done: false
+            id: uuid(),
+            message: "Testandinho 2",
+            done: true
         }
     ]
 
-    const [todoList, setTodoList] = useState(mock)
+    const [todoList, setTodoList] = useState<Todo[]>([])
+    const [todo, setTodo] = useState("")
+
+    function addTodo() {
+        const newTodo = {
+            id: uuid(),
+            message: todo,
+            done: false
+        }
+        setTodoList([newTodo, ...todoList])
+    }
+
+    useEffect(() => {
+        const todos = localStorage.getItem('todoList')
+
+        if(todos){
+            setTodoList(JSON.parse(todos))
+        }
+    },[])
+
+    useEffect(() => {
+        localStorage.setItem('todoList',JSON.stringify(todoList))
+    },[todoList])
 
     return (
         <div id="todoList">
             <h1>TodoList da felicidade</h1>
 
             <div className="todoInput">
-                <input type="text"/>
-                <button type="button" className="addTodo">Add</button>
+                <input 
+                    type="text" 
+                    value={todo} 
+                    onChange={ e => setTodo(e.target.value) }
+                />
+                <button 
+                    type="button" 
+                    className="addTodo" 
+                    onClick={addTodo}
+                >
+                    Add
+                </button>
             </div>
 
             <div>
                 {
                     todoList.map(todo => (
-                        <div className="todo">
+                        <div className="todo" key={todo.id}>
                             <div>{todo.message}</div>
                             <div className="actions">
+
                                 {
                                     todo.done ? (
                                         <button type="button" className="icon">
